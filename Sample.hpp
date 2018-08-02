03b181c648f7f5d2d46af6a71267c40e284a7e2a
nikudyshko <lishuk@i.ua>
2018-08-02 21:53:21 +0300

    Change "struct" to "calss"
    Changed "struct" to "class" for convenience

--- Sample.hpp
+++ Sample.hpp
@@ -4,18 +4,19 @@
 //necessary includes, armadillo - for linear algebra 
 #include <armadillo> 
 
-//implements Sample structure, that represents train or validation sample 
-struct Sample
+//implements Sample class that represents train or validation sample 
+class Sample
 { 
+public: 
 	//in - input data 
-	//out - corresponding to in output data 
+	//out - corrseponding to in output data 
 	arma::vec in, out; 
-	//~Sample() - destructor, that clears memory of vectors 
+	//~Sample() - destructor that clears memory of vectors 
 	~Sample()
 	{
 		in.clear(); 
 		out.clear(); 
 	}
-};
+}; 
 
 #endif 
