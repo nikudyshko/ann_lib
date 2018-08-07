8052561c694d548890d64415dfbcfe20420fc3ae
nikudyshko <lishuk@i.ua>
2018-08-08 02:49:06 +0300

    Remove Dense class
    Removed Dense class. All necessary data now contained in NNetwork class. Rewritten some function members of NNetwork

--- Layer.hpp
+++ Layer.hpp
@@ -1,5 +1,3 @@
-#pragma once 
-
 #ifndef LAYER_H 
 #define LAYER_H 
 
@@ -7,92 +5,6 @@
 #include <random>
 #include <armadillo> 
 #include "Activation.hpp"
-
-class Dense
-{ 
-private: 
-	friend class NNetwork; 
-	size_t num_neurons; 
-	const double bias_const = 0.1; 
-	arma::vec biases, grad_b; 
-	arma::mat weights, grad_w; 
-	arma::mat jacoby; 
-
-	Activation activative; 
-	Derivative derivative; 
-
-	void zero_weights(); 
-	void init_weights(); 
-public: 
-	arma::vec input, error; 
-	arma::vec logit, activ, delta; 
-	void forward(arma::vec const &input_); 
-	void backward(arma::vec const &error_); 
-
-	Dense(size_t previous_num_neurons, size_t current_num_neurons, std::string const &fun_name); 
-	~Dense(); 
-}; 
-
-Dense::Dense(size_t previous_num_neurons, size_t current_num_neurons, std::string const &fun_name)
-{
-	this->num_neurons = current_num_neurons;
-	this->input.zeros(previous_num_neurons);
-	this->error.zeros(current_num_neurons);
-	this->logit.zeros(current_num_neurons);
-	this->activ.zeros(current_num_neurons);
-	this->delta.zeros(current_num_neurons);
-	this->biases.zeros(current_num_neurons);
-	this->grad_b.zeros(current_num_neurons);
-	this->weights.zeros(current_num_neurons, previous_num_neurons);
-	this->grad_w.zeros(current_num_neurons, previous_num_neurons);
-	this->jacoby.zeros(current_num_neurons, current_num_neurons); 
-	this->activative = Functions::activative(fun_name); 
-	this->derivative = Functions::derivative(fun_name); 
-} 
-
-Dense::~Dense() 
-{
-	this->input.clear(); 
-	this->error.clear(); 
-	this->logit.clear(); 
-	this->activ.clear(); 
-	this->delta.clear(); 
-	this->biases.clear(); 
-	this->grad_b.clear(); 
-	this->weights.clear(); 
-	this->grad_w.clear(); 
-	this->jacoby.clear(); 
-}
-
-void Dense::init_weights()
-{ 
-	std::mt19937 gen(time(NULL)); 
-	std::normal_distribution<double> dist(0.0, std::sqrt(1 / static_cast<double>(this->weights.n_cols))); 
-	this->biases.fill(this->bias_const); 
-	this->weights.imbue(dist(gen)); 
-} 
-
-void Dense::zero_weights()
-{ 
-	this->grad_b.zeros(); 
-	this->grad_w.zeros(); 
-} 
-
-void Dense::forward(arma::vec const &input_)
-{ 
-	this->input = input_; 
-	this->logit = this->weights*this->input + this->biases; 
-	this->activative(this->logit, this->activ); 
-} 
-
-void Dense::backward(arma::vec const &error_)
-{
-	this->derivative(this->activ, this->jacoby);
-	this->delta = this->jacoby*error_;
-	this->error = this->weights.t()*this->delta; 
-	this->grad_b += this->delta; 
-	this->grad_w += this->delta*this->input.t(); 
-} 
 
 class Loss
 {
--- NNetwork.hpp
+++ NNetwork.hpp
@@ -1,3 +1,5 @@
+#pragma once 
+
 #ifndef NNETWORK_H
 #define NNETWORK_H 
 
@@ -17,23 +19,48 @@
 #include "Layer.hpp" 
 #include "Sample.hpp" 
 
+class HyperP
+{
+public: 
+	size_t num_neurons; 
+	std::string fun_name; 
+	~HyperP() { this->fun_name.clear(); }
+};
+
 //implementing of neural network 
 class NNetwork
 {
 private: 
+	static constexpr double b1 = 0.9, b2 = 0.999, eps = 1e-8; 
+	static constexpr double bias_const = 0.1; 
 	//num_layers - number of layers in neural network 
 	//train_set_size - size of the train set :) 
 	//validation_set_size - size of the validation set :) 
 	size_t num_layers, train_set_size, validation_set_size; 
 	//architect - contains description of neural network's architecture 
 	//in form (number of neurons, name of activation function) 
-	std::vector< std::pair< size_t, std::string > > architect; 
-	//network - contains a Layers of neural network; 
-	std::vector< Dense > network; 
-	Loss loss_fun; 
+	std::default_random_engine shuffle_gen, rand_gen; 
+	std::uniform_int_distribution<size_t> dist_tr, dist_vl; 
+
+	std::vector< HyperP > architect; 
 	//train_set - array of samples for training neural network 
 	//validation_set - array of samples for testing neural network 
-	std::vector< Sample > train_set, validation_set; 
+	std::vector< Sample* > train_set, validation_set; 
+	std::vector< Activation > a_funs; 
+	std::vector< Derivative > d_funs; 
+
+	Loss loss_fun; 
+
+	arma::field< arma::vec > logits, activs, deltas; 
+	arma::field< arma::vec > biases, grad_b; 
+	arma::field< arma::vec > b_m, b_m_old; 
+	arma::field< arma::vec > b_v, b_v_old; 
+	arma::field< arma::mat > weights, grad_w; 
+	arma::field< arma::mat > w_m, w_m_old; 
+	arma::field< arma::mat > w_v, w_v_old; 
+	arma::field< arma::mat > jacobies; 
+
+	Loss loss_fun; 
 
 	//zero_gradient() - fills gradient fields with zeros 
 	void zero_gradient(); 
@@ -87,29 +114,60 @@ public:
 }; 
 
 void NNetwork::zero_gradient()
-{
-	for (size_t i = 0; i < this->num_layers; ++i)
-		this->network[i].zero_weights(); 
+{ 
+	for (size_t i = 1; i < this->num_layers; ++i)
+	{ 
+		this->grad_b[i].zeros(); 
+		this->grad_w[i].zeros(); 
+	} 
 } 
 
 void NNetwork::feed_forward(arma::vec const &in)
 { 
-	this->network[0].forward(in); 
+	this->activs[0] = in; 
 	for (size_t i = 1; i < this->num_layers; ++i)
-		this->network[i].forward(this->network[i - 1].activ); 
+	{
+		this->logits[i] = this->weights[i] * this->activs[i - 1] + this->biases[i]; 
+		this->a_funs[i](this->logits[i], this->activs[i]); 
+	} 
 } 
 
 void NNetwork::back_propagation(arma::vec const &out)
-{
-	this->loss_fun.backward(this->network[this->num_layers - 1].activ, out); 
-	this->network[this->num_layers - 1].backward(this->loss_fun.backward); 
-	for (size_t i = this->num_layers - 2; i >= 0; --i)
-		this->network[i].backward(this->network[i + 1].error); 
+{ 
+	this->loss_fun.backward(this->activs[this->num_layers - 1], out); 
+	this->d_funs[this->num_layers - 1](this->activs[this->num_layers - 1], this->jacobies[this->num_layers - 1]); 
+	this->deltas[this->num_layers - 1] = this->jacobies[this->num_layers - 1] * this->loss_fun.error; 
+	this->grad_b[this->num_layers - 1] = this->deltas[this->num_layers - 1]; 
+	this->grad_w[this->num_layers - 1] = this->deltas[this->num_layers - 1] * this->activs[this->num_layers - 2]; 
+	for (size_t i = this->num_layers - 2; i >= 1; --i)
+	{ 
+		this->d_funs[i](this->activs[i], this->jacobies[i]); 
+		this->deltas[i] = this->jacobies[i] * this->weights[i + 1].t()*this->deltas[i + 1]; 
+		this->grad_b[i] += this->deltas[i]; 
+		this->grad_w[i] += this->deltas[i] * this->activs[i - 1].t(); 
+	} 
 } 
 
 void NNetwork::clear_neural_net()
 { 
-	this->network.erase(this->network.begin(), this->network.end()); 
+	this->logits.clear(); 
+	this->activs.clear(); 
+	this->deltas.clear(); 
+	this->biases.clear(); 
+	this->grad_b.clear(); 
+	this->b_m.clear(); 
+	this->b_v.clear(); 
+	this->b_m_old.clear(); 
+	this->b_v_old.clear(); 
+	this->weights.clear(); 
+	this->grad_w.clear(); 
+	this->w_m.clear(); 
+	this->w_v.clear(); 
+	this->w_m_old.clear(); 
+	this->w_v_old.clear(); 
+	this->jacobies.clear(); 
+
+	this->architect.erase(this->architect.begin(), this->architect.end()); 
 } 
 
 void NNetwork::clear_train_set()
@@ -123,26 +181,32 @@ void NNetwork::clear_validation_set()
 } 
 
 void NNetwork::init_weights()
-{
-	for (size_t i = 0; i < this->num_layers; ++i)
-		this->network[i].init_weights(); 
+{ 
+	std::mt19937 gen(time(NULL)); 
+
+	for (size_t i = 1; i < this->num_layers; ++i)
+	{
+		std::normal_distribution<double> dist(0.0, std::sqrt(1.0 / this->architect[i].num_neurons)); 
+		this->weights[i].imbue([&gen, &dist]() {return dist(gen); }); 
+		this->biases[i].fill(this->bias_const); 
+	} 
 } 
 
 void NNetwork::train_step(int iter, size_t batch_num, size_t batch_size, double learning_rate, double reg_rate) 
-{
+{ 
 	this->zero_gradient(); 
-	for(size_t i = 0; i < batch_size; i++) 
+	for (size_t i = 0; i < batch_size; ++i)
 	{
-		this->feed_forward(this->train_set[batch_num*batch_size + i].in); 
-		this->back_propagation(this->train_set[batch_num*batch_size + i].out); 
+		this->feed_forward(this->train_set[batch_num*batch_size + i]->in); 
+		this->back_propagation(this->validation_set[batch_num*batch_size + i]->out); 
 	} 
 	this->update_weights(iter, batch_size, learning_rate, reg_rate); 
 } 
 
 arma::vec& NNetwork::predict(arma::vec const &in)
-{
+{ 
 	this->feed_forward(in); 
-	return this->network[this->num_layers - 1].activ; 
+	return this->activs[this->num_layers - 1]; 
 } 
 
 double NNetwork::calc_accuracy(size_t batch_size, bool full_set = false) 
@@ -150,28 +214,25 @@ double NNetwork::calc_accuracy(size_t batch_size, bool full_set = false)
 	size_t begin = 0, end = 0; 
 	double acc = 0.0; 
 
-	if (full_set) 
+	if (full_set)
 	{
 		begin = 0; 
 		end = this->validation_set_size; 
 	} 
 	else
 	{
-		std::default_random_engine gen(time(NULL)); 
-		std::uniform_int_distribution<size_t> dist(0, this->validation_set_size - batch_size - 1); 
-
-		begin = dist(gen); 
-		end = begin + batch_size; 
+		begin = this->dist_vl(rand_gen);
+		end = begin + batch_size;
 	} 
 
-	for (size_t i = 0; i < this->validation_set_size; ++i)
+	for (size_t i = begin; i < end; ++i)
 	{
-		this->feed_forward(this->validation_set[i].in); 
-		if (this->network[this->num_layers - 1].activ.index_max() == this->validation_set[i].out.index_max())
+		this->feed_forward(this->validation_set[i]->in); 
+		if (this->activs[this->num_layers - 1].index_max() == this->validation_set[i]->out.index_max())
 			acc += 1.0; 
 	} 
 
-	return acc / static_cast<double>(this->validation_set_size); 
+	return acc / static_cast<double>(end - begin); 
 } 
 
 double NNetwork::calc_loss(size_t batch_size, bool full_set = false) 
@@ -186,21 +247,18 @@ double NNetwork::calc_loss(size_t batch_size, bool full_set = false)
 	} 
 	else
 	{
-		std::default_random_engine gen(time(NULL)); 
-		std::uniform_int_distribution<size_t> dist(0, this->train_set_size - batch_size - 1); 
-
-		begin = dist(gen); 
+		begin = this->dist_tr(rand_gen); 
 		end = begin + batch_size; 
 	} 
 
-	for (size_t i = 0; i < this->train_set_size; ++i)
+	for (size_t i = begin; i < end; ++i)
 	{
-		this->feed_forward(this->train_set[i].in); 
-		this->loss_fun.forward(this->network[this->num_layers - 1].activ, this->train_set[i].out); 
+		this->feed_forward(this->train_set[i]->in); 
+		this->loss_fun.forward(this->activs[this->num_layers - 1], this->train_set[i]->out); 
 		loss += this->loss_fun.loss; 
 	} 
 
-	return loss / static_cast<double>(this->train_set_size); 
+	return loss / static_cast<double>(end - begin); 
 }
 
 #endif
